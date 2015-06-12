c6946e9f6c754db44743d4a31ddac8d80f53704e
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-11 20:13:50 -0500

    index h1

--- index.php
+++ index.php
@@ -1,26 +1,51 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
 
-<section class="slider">
-	<div class="flexslider">
-  		<ul class="slides">
-    		<li>
-    	    	<img src="/_assets/img/slider-girls.jpg" />
-    		</li>
-    		<li>
-    	    	<img src="/_assets/img/slider-girls.jpg" />
-    		</li>
-    		<li>
-    	    	<img src="/_assets/img/slider-girls.jpg" />
-    		</li>
-    		<li>
-    	    	<img src="/_assets/img/slider-girls.jpg" />
-    		</li>
-        </ul>
-        <div class="gradient"></div>
-	</div>
-</section>
+<div id="home">
+
+	<section class="slider">
+	
+		<div class="flexslider">
+	
+	  		<ul class="slides">
+	
+	    		<li>
+	    	    	<img src="/_assets/img/slider-girls.jpg" />
+	    		</li>
+	    		<li>
+	    	    	<img src="/_assets/img/slider-girls.jpg" />
+	    		</li>
+	    		<li>
+	    	    	<img src="/_assets/img/slider-girls.jpg" />
+	    		</li>
+	    		<li>
+	    	    	<img src="/_assets/img/slider-girls.jpg" />
+	    		</li>
+	
+	        </ul>
+	
+	        <div class="gradient"></div>
+
+	        <div class="h1-container"><h1>On a foundation of Christian faith, Martha O'Bryan Center empowers children, youth, and adults in poverty to transform their lives through work, education, employment and fellowship.</h1></div>
+	
+		</div>
+	
+	</section>
+
+	<section id="we">
+		
+		<div class="inner">
+			
+			<article class="three">
+				
 
 
+			</article><!--three-->
+
+		</div><!--inner-->
+
+	</section><!--we-->
+
+</div><!--home-->
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
