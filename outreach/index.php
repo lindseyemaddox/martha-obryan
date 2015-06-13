06de929f6ad75bc5c886f56303010faceed671e4
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-13 16:09:50 -0500

    outreach landing page

--- outreach/index.php
+++ outreach/index.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<script src="/_assets/js/isotope.jquery.min.js"></script>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
 
 <div id="outreach" class="sub">
@@ -15,6 +16,194 @@
 
 	</section><!--header-->
 
+	<section id="options">
+		
+		<div class="inner">
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque urna neque, lacinia nec risus non, consectetur hendrerit lectus. Praesent in sagittis ligula. Suspendisse potenti. Duis ac fermentum mi, quis suscipit enim. Integer sed velit nec justo pharetra elementum a sit amet justo. Aenean quis lorem condimentum, maximus arcu sed, viverra lacus. Quisque nibh metus, convallis vel diam fringilla, facilisis luctus nibh.</p>
+
+			<div class="wrapper">
+
+			    <div class="main">
+
+			        <div class="container">
+				
+	  			    	<aside class="sorter">
+	  			    		
+	  			      		<div id="sidebar" class="clearfix">
+
+	  			        		<ul id="filters" class="option-set clearfix" data-option-key="filter">
+	  			          			<li><h3><a href="#filter" data-option-value="*" class="selected">Show All</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".community">Clinical Services, Community, and Families</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".early-learning">Early Learning Center</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".k8-education">K-8 Education</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".high-school">High School Education and Academic Student Unions</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".college">College and Career</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".high-performers">High Performing Schools</a></h3></li>
+	  			          			<li><h3><a href="#filter" data-option-value=".other">Other</a></h3></li>
+    							</ul>
+
+    						</div><!--sidebar-->
+
+    			    	</aside>
+
+    			    	<div id="items" class="clearfix">
+
+    						<article class="item community">
+    						  <h2>Community Engagement</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/community/engagement/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item community">
+    						  <h2>Family Engagement</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/community/family/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item community">
+    						  <h2>Counseling &amp; Pastoral Care</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/community/counseling/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item early-learning">
+    						  <h2>Early Learning Center</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/early-learning/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item k8-education">
+    						  <h2>K-8 Education</h2>
+    						  <h4>Formerly known as THRIVE and RAP, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/k8-education/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item high-school">
+    						  <h2>Top Floor at Stratford</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/high-school/top-floor/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item high-school">
+    						  <h2>College Zone at Maplewood</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/high-school/college-zone/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item high-school">
+    						  <h2>Nashville After Zone Alliance</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/high-school/after-zone/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item college">
+    						  <h2>Post-Secondary Success Initiative</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/college/post-secondary/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item college">
+    						  <h2>Chapter 2</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/college/chapter-2/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item college">
+    						  <h2>Adult Education</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/college/adult-education/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item high-performers">
+    						  <h2>East End Preparatory School</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/high-performers/east-end-prep/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item high-performers">
+    						  <h2>Explore! Community School</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/high-performers/explore/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item other">
+    						  <h2>Summer Social Justice Institute</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/other/social-justice-institute/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item other">
+    						  <h2>Recruit 2 Mission</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/other/recruit-2/" class="btn">Learn More</a>
+    						</article>
+
+    						<article class="item other">
+    						  <h2>Break the Line podcast</h2>
+    						  <h4>Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget nibh a justo.</h4>
+    						  <a href="/outreach/other/break-the-line/" class="btn">Learn More</a>
+    						</article>
+
+    				    </div><!--items-->
+
+        		    </div><!--container-->
+
+        		</div><!--main-->
+
+	        </div><!--wrapper-->
+
+	    </div><!--inner-->
+
+	</section><!--options-->
+
 </div><!--outreach-->
+
+<script>
+
+    $(function(){
+
+      var $container = $('#items');
+
+      $container.isotope({
+        itemSelector : '.item'
+      });
+
+
+      var $optionSets = $('#sidebar .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+      $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+
+        return false;
+      });
+
+    });
+
+
+</script>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
