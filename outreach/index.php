91c6e23b56a952c8597551ba817935f79b25f9df
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-13 13:01:16 -0500

    sub header build

--- outreach/index.php
+++ outreach/index.php
@@ -1,11 +1,17 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
 
-<div id="outreach">
+<div id="outreach" class="sub">
 
 	<section class="header">
 	
+		<div class="gradient"></div>
 
+		<div class="inner">
+			
+			<h1>Outreach</h1>
+
+		</div><!--inner-->
 
 	</section><!--header-->
 
