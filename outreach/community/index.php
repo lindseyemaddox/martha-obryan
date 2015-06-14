d86f9b01cead9a5bbf54c287be7b9ccfa51dc8df
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-14 00:45:33 -0500

    half of community page, still no subnav

--- outreach/community/index.php
+++ outreach/community/index.php
@@ -1,13 +1,71 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
 
-<div id="outreach">
+<div id="outreach" class="sub">
 
 	<section class="header">
 	
+		<div class="gradient"></div>
 
+		<div class="inner">
+			
+			<h1>Outreach</h1>
+
+		</div><!--inner-->
 
 	</section><!--header-->
+
+	<section class="subpage">
+
+		<div class="inner">
+		
+			<h2>Clinical Services, Community, and Families</h2>
+
+			<h3>Stories of Exceptional Youth, Strong Families, Full Lives</h3>
+
+			<p>The Martha O'Bryan Center is located in Nashville's poorest neighborhood. The daily challenges are unique and diverse for the 6,000 children and adults who show up at our door each year. There are just as many stories. Here are a few:</p>
+
+			<p>The “Highway of Hope” at the Martha O'Bryan Center is a string of services that supports and follows young children, youth and their families from cradle to college to career. Services include classes for new and expecting parents, child care, afterschool academic and enrichment programs for youth, GED classes and pretests and Work Ready programs for adults. This movie highlights the potential for the families served by Martha O'Bryan Center to become inspired, productive citizens.</p>
+
+			<div class="videowrap">
+		
+				<div class="video widescreen">
+		
+					<iframe src="https://player.vimeo.com/video/15643219" width="100%" height="562" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		
+				</div><!--video-->
+		
+			</div><!--videowrap-->
+
+			<h3>Family Overcomes Obstacles</h3>
+
+			<p>Cassandra and James Berry never expected to end up in public housing. But two years ago, James lost his job. They had two children and a third on the way, and they had no income or resources. Soon they were evicted from their home, and ended up at Cayce Place, the largest, oldest and poorest public housing development in Nashville.</p>
+
+			<p>As with all new residents at Cayce, the Berrys received a welcome from the Martha O’Bryan Center – a laundry basket filled with household essentials like cleaning products, trash sacks, laundry detergent, shampoo, toilet paper, hand lotion and more. They learned more about the Center and attended the Tied Together parenting education program. And the more they learned, the more they became involved.</p>
+
+			<p>James enrolled in Center’s GED class and found a job at a restaurant thanks to the Center’s Work Ready program. Inspired by the Center’s staff and mission, Cassandra now is pursuing a degree in social work at Nashville State.</p>
+
+			<p>Their oldest son, Michael, 10, was one of 100 youth to participate in Martha O'Bryan Center's Summer Camp 2010. Saundra, 5, was in the Early Learning Center until she started kindergarten this fall. And their youngest, James Jr., continues to thrive in the ELC while mom is at school and dad is at work.</p>
+
+			<p>“It’s a lot of people that fell on hard times, and they don’t have the money or the resources to do things,” said James Sr. “Martha O'Bryan makes a way out of no way, and I thank God for it. They’ve helped me be a better role model for my family, a better role model as a husband, a father, a man of God, and I thank God for everything." </p>
+
+			<p>“We’re not trying to stay in James Cayce forever,” said Cassandra. “We’re just trying to use that just for the moment, to get on our feet and do what we’ve got to do. It’s (Martha O’Bryan Center) a valuable resource and if somebody is not using it, they need to use it.”</p>
+
+	    </div><!--inner-->
+
+	</section><!--subpage-->
+
+	<section id="crumbs">
+		
+		<div class="inner">
+			
+			<div class="left"><a href="/outreach/">&lt; Outreach</a></div><!--left-->
+
+			<div class="right"><a href="/outreach/community/engagement/">Community Engagement &gt;</a></div><!--right-->
+
+		</div><!--inner-->
+
+	</section><!--crumbs-->
 
 </div><!--outreach-->
 
