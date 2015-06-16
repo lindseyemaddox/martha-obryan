52d322fd387a98afc664f8ea955d22ecfd8eb4f0
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-15 21:56:47 -0500

    moved full nav to main nav

---
+++ _assets/inc/subnav-outreach.php
@@ -0,0 +1,56 @@
+<script type="text/javascript" src="/_assets/js/subnavstyler.js"></script>
+
+<section id="subnav-cont">
+
+	<div class="inner">
+	
+		<ul id="subnav">
+			<li><a href="/outreach/" class="h1"><h1>Outreach</h1></a>
+				<ul>
+					<li><a href="/outreach/community/">Clinical Services, Community, and Families</a>
+						<ul>
+							<li><a href="/outreach/community/engagement/">Community Engagement</a>
+								<ul>
+									<li><a href="/outreach/community/engagement/promise-neighborhood/">Promise Neighborhood</a></li>
+								</ul>
+							</li>
+							<li><a href="/outreach/community/family/">Family Engagement</a></li>
+							<li><a href="/outreach/community/counseling/">Counseling &amp; Pastoral Care</a></li>
+						</ul>
+					</li>
+					<li><a href="/outreach/early-learning/">Early Learning Center</a></li>
+					<li><a href="/outreach/k8-education/">K-8 Education</a></li>
+					<li><a href="/outreach/high-school/">High School Education and Academic Student Unions</a>
+						<ul>
+							<li><a href="/outreach/high-school/top-floor/">Top Floor at Stratford</a></li>
+							<li><a href="/outreach/high-school/college-zone/">College Zone and Maplewood</a></li>
+							<li><a href="/outreach/high-school/after-zone/">Nashville After Zone Alliance</a></li>
+						</ul>
+					</li>
+					<li><a href="/outreach/college/">College and Career</a>
+						<ul>
+							<li><a href="/outreach/college/post-secondary/">Post-Secondary Success Initiative</a></li>
+							<li><a href="/outreach/college/chapter-2/">Chapter 2</a></li>
+							<li><a href="/outreach/college/adult-education/">Adult Education</a></li>
+						</ul>
+					</li>
+					<li><a href="/outreach/high-performers/">High Performing Schools</a>
+						<ul>
+							<li><a href="/outreach/high-performers/east-end-prep/">East End Preparatory School</a></li>
+							<li><a href="/outreach/high-performers/explore/">Explore! Community School</a></li>
+						</ul>
+					</li>
+					<li><a href="/outreach/other/">Other Outreach</a>
+						<ul>
+							<li><a href="/outreach/other/social-justice-institute/">Summer Social Justice Institute</a></li>
+							<li><a href="/outreach/other/recruit-2/">Recruit 2 Mission</a></li>
+							<li><a href="/outreach/other/break-the-line/">Break the Line podcast</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+		</ul>
+
+	</div><!--inner-->
+
+</section><!--subnav-->
