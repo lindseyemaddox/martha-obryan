aaeca8d261fe74a43e586bc3eb8ff2a5f340ac5e
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-24 00:11:19 -0500

    complete news page

--- news/index.php
+++ news/index.php
@@ -34,7 +34,33 @@
 
 				<h6>Timothy Cunningham and George Gutierrez, 2012 Stratford High School graduates and alumni of the Top Floor @ Stratford program, are rising juniors at Middle Tennessee State University. This summer... <a class="readmore fancybox fancybox.iframe" href="/news/news-items/news-item.php">read more</a></h6>
 
+				<h4>Support our College Students at College-Bound Bash</h4>
+
+				<h5>6/23/2014</h5>
+
+				<h6>NextUP-Martha O’Bryan Center, our young professionals group, is leading the leading the drive to support our students. Help students eliminate financial barriers to college with dorm-room and classroom essentials... <a class="readmore fancybox fancybox.iframe" href="/news/news-items/news-item.php">read more</a></h6>
+
+				<h4>Persistence Pays Off for Adult Education Student</h4>
+
+				<h5>5/12/2014</h5>
+
+				<h6>Lynne Childress worked hard to finish her high school equivalency degree while continuing to work for Wee Care Childcare in Nashville. After several years of persistence, she passed the... <a class="readmore fancybox fancybox.iframe" href="/news/news-items/news-item.php">read more</a></h6>
+
+				<h4>Toastmasters Teach Adult Education Students</h4>
+
+				<h5>3/4/2014</h5>
+
+				<h6>A team of Toastmasters volunteers from HealthTrust recently completed the final session of a six-week public speaking and leadership curriculum for Martha O'Bryan Center Adult Education students. The group... <a class="readmore fancybox fancybox.iframe" href="/news/news-items/news-item.php">read more</a></h6>
+
+				<h4>Sales of Edgehill Cafe's 'Drink of the Month' to go to Martha O'Bryan</h4>
+
+				<h5>1/29/2014</h5>
+
+				<h6>Announcing a delicious new partnership: Edgehill Cafe in Nashville will donate all sales of its "Drink of the Month" to the Martha O'Bryan Center.  For February, the drink is called "Give... <a class="readmore fancybox fancybox.iframe" href="/news/news-items/news-item.php">read more</a></h6>
+
 			</div><!--news-items-->
+
+			<div id="pagination"><a href="/news/">&lt;</a> <a class="active" href="/news/">1</a> <a href="/news/">2</a> <a href="/news/">3</a> <a href="/news/">4</a> <a href="/news/">5</a> <a href="/news/">&gt;</a></div>
 
 	    </div><!--inner-->
 
