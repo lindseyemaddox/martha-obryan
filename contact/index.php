6a0792f9b7bbb9de2cfea4d2c808f15e664b0191
lindseyemaddox <lindseyemaddox@gmail.com>
2015-07-01 01:06:22 -0500

    contact gateway page

--- contact/index.php
+++ contact/index.php
@@ -37,11 +37,92 @@
 				
 				<h4>Sign Up to Receive the Newsletter</h4>
 
-				<h5>Receive news about events, volunteering and people at Martha O'Bryan Center</h5>
+				<h5>Receive news about events, volunteering and people at Martha O'Bryan Center.</h5>
 
 				<a class="btn">Contact Us</a>
 
 			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>Individual or Group Volunteers</h4>
+
+				<h5>Would you like to help? We welcome both individual and group volunteers.</h5>
+
+				<a class="btn">Volunteer</a>
+
+			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>Schedule a Visit to Martha O’Bryan Center</h4>
+
+				<h5>Each session includes a history and overview of the work of the Martha O'Bryan Center.</h5>
+
+				<a class="btn">Book Tour</a>
+
+			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>Donate to Martha O’Bryan Center</h4>
+
+				<h5>We are grateful to all who support Martha O'Bryan by making a tax-deductible gift.</h5>
+
+				<a class="btn">Donate Now</a>
+
+			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>Become a Martha O’Bryan Center Patron</h4>
+
+				<h5>We value the security of our donors and strive to safeguard your information.</h5>
+
+				<a class="btn">Patronage</a>
+
+			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>Leave Volunteer Feedback</h4>
+
+				<h5>Please take 5-7 minutes to let us know about your volunteer experience.</h5>
+
+				<a class="btn">Feedback</a>
+
+			</article><!--contact-item-->
+
+			<article class="contact-item">
+				
+				<h4>View our Wish Lists</h4>
+
+				<h5>Martha O’Bryan Center maintains several wish lists that help support our center.</h5>
+
+				<a class="btn">Feedback</a>
+
+			</article><!--contact-item-->
+
+			<div class="contact-info">
+				
+				<article id="address">
+					
+				<h6>Martha O'Bryan Center<br/>
+				<a href="https://www.google.com/maps/place/711+S+7th+St,+Nashville,+TN+37206/@36.1664483,-86.75512,17z/data=!3m1!4b1!4m2!3m1!1s0x88646630dd5f4d3d:0x1948db65d0e42022" target="_blank">711 South Seventh Street<br/>
+				Nashville, TN 37206-3895</a><br/><br/>
+
+				Phone: <a href="tel:16152541791" target="_blank">(615) 254-1791</a><br/>
+				Fax: (615) 242-3411</h6>
+
+				</article><!--address-->
+
+				<article id="map">
+					
+					<a href="https://www.google.com/maps/place/711+S+7th+St,+Nashville,+TN+37206/@36.1664483,-86.75512,17z/data=!3m1!4b1!4m2!3m1!1s0x88646630dd5f4d3d:0x1948db65d0e42022" target="_blank"><img src="/_assets/img/map.jpg" title="map" alt="map"></a>
+
+				</article><!--map-->
+
+			</div><!--contact-info-->
 
 	    </div><!--inner-->
 
@@ -51,9 +132,9 @@
 		
 		<div class="inner">
 			
-			<div class="left"><a href="/news/newsletters/">&lt; Newsletters</a></div><!--left-->
+			<div class="left"><a href="/news/">&lt; News &amp; Events</a></div><!--left-->
 
-			<div class="right"><a href="/store/">Store &gt;</a></div><!--right-->
+			<div class="right"><a href="/privacy">Privacy &amp; Transparency Statement &gt;</a></div><!--right-->
 
 		</div><!--inner-->
 
